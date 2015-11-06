ce7f76acdbadf0ad05935d99070a2447b30375f5
David Zeng <soo.ne@hotmail.com>
2015-11-05 20:06:06 -0500

    D.Z. - combined refresh and draw into main

--- src/main.js
+++ src/main.js
@@ -38,5 +38,38 @@ window.onload = function main(){
 	loop();
 
 	window.requestAnimationFrame(loop,canvas);
-	
+
 };
+
+/**
+* \brief Calls functions to update and draw players and objects on planets
+*
+*/
+
+	var loop = function() {
+		update();
+		draw();
+
+		//Loop about 60 fps.
+		window.requestAnimationFrame(loop,canvas);
+	};
+
+	var init = function() {
+
+	};
+
+	var draw = function() {
+
+	};
+
+	var update = function() {
+		var modifier = 10;
+		updateEnvironmentMovement();
+		updatePlayerMovement(modifier);
+	};
+
+
+	var draw = function() {
+
+
+	};
